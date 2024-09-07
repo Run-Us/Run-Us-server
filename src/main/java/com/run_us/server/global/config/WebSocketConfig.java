e53915bf6f69182bb1b9738c6f213d6973613cea
jeeheaG <jeehea99@naver.com>
2024-09-07 12:29:47 +0900

    :bulb: comment : add javadoc description

--- src/main/java/com/run_us/server/global/config/WebSocketConfig.java
+++ src/main/java/com/run_us/server/global/config/WebSocketConfig.java
@@ -6,6 +6,9 @@ import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBr
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
+/**
+ * websocket config 클래스
+ */
 @Configuration
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
