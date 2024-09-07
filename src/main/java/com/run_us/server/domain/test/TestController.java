1c2ebe52c3ac37a688b068f03db0ddac3070d71c
jeeheaG <jeehea99@naver.com>
2024-09-07 12:30:32 +0900

    :sparkles: feat : make TestController and ws init test method

---
+++ src/main/java/com/run_us/server/domain/test/TestController.java
@@ -0,0 +1,28 @@
+package com.run_us.server.domain.test;
+
+import com.run_us.server.global.common.SuccessResponse;
+import com.run_us.server.global.exceptions.enums.ExampleErrorCode;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.stereotype.Controller;
+
+/**
+ * 테스트용 api 컨트롤러
+ */
+@Controller
+public class TestController {
+    private final SimpMessagingTemplate simpMessagingTemplate;
+
+    public TestController(SimpMessagingTemplate simpMessagingTemplate) {
+        this.simpMessagingTemplate = simpMessagingTemplate;
+    }
+
+    /**
+     * websocket 초기 세팅 테스트용 요청
+     */
+    @MessageMapping("/test/hello")
+    public void initTest() {
+        //TODO : 소켓 응답 코드에 대한 논의 필요. 임시로 EXAMPLE 응답
+        simpMessagingTemplate.convertAndSend("/hello", SuccessResponse.messageOnly(ExampleErrorCode.EXAMPLE));
+    }
+}
