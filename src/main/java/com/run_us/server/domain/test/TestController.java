d670a3107630184d4890545674377fda17e5089a
jeeheaG <jeehea99@naver.com>
2024-09-09 02:15:12 +0900

    :memo: chore : add log for initTest() in TestController

--- src/main/java/com/run_us/server/domain/test/TestController.java
+++ src/main/java/com/run_us/server/domain/test/TestController.java
@@ -2,6 +2,7 @@ package com.run_us.server.domain.test;
 
 import com.run_us.server.global.common.SuccessResponse;
 import com.run_us.server.global.exceptions.enums.ExampleErrorCode;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
@@ -9,6 +10,7 @@ import org.springframework.stereotype.Controller;
 /**
  * 테스트용 api 컨트롤러
  */
+@Slf4j
 @Controller
 public class TestController {
     private final SimpMessagingTemplate simpMessagingTemplate;
@@ -24,5 +26,6 @@ public class TestController {
     public void initTest() {
         //TODO : 소켓 응답 코드에 대한 논의 필요. 임시로 EXAMPLE 응답
         simpMessagingTemplate.convertAndSend("/hello", SuccessResponse.messageOnly(ExampleErrorCode.EXAMPLE));
+        log.info("initTest : /topic/test/hello");
     }
 }
