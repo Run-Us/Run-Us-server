5dd72af9e7320916617a61df8597a2f38332a66f
ekgns33 <ekgns00421@gmail.com>
2024-09-14 23:55:19 +0900

    ✅ Test : add user fixtures, apply to testcodes

---
+++ src/test/java/com/run_us/server/domain/user/model/UserFixtures.java
@@ -0,0 +1,24 @@
+package com.run_us.server.domain.user.model;
+
+import java.time.LocalDate;
+
+public final class UserFixtures {
+
+  public static final LocalDate DEFAULT_BIRTH_DATE = LocalDate.of(1999, 1, 1);
+
+  public static User getDefaultUser() {
+    return User.builder()
+        .nickname("NICKNAME")
+        .birthDate(DEFAULT_BIRTH_DATE)
+        .gender(Gender.NONE)
+        .build();
+  }
+
+  public static User getDefaultUserWithNickname(String nickname) {
+    return User.builder()
+        .nickname(nickname)
+        .birthDate(DEFAULT_BIRTH_DATE)
+        .gender(Gender.NONE)
+        .build();
+  }
+}
