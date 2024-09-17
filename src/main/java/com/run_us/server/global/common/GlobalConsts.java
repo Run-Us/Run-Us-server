d9a8a6d01861b2071e22e7715b3aa8b5f0410ebb
jeeheaG <jeehea99@naver.com>
2024-09-18 04:14:02 +0900

    :sparkles: feat : make RunningSocketController and startRunning (test not yet)

--- src/main/java/com/run_us/server/global/common/GlobalConsts.java
+++ src/main/java/com/run_us/server/global/common/GlobalConsts.java
@@ -3,4 +3,6 @@ package com.run_us.server.global.common;
 public final class GlobalConsts {
   public static final String TIME_ZONE_ID = "Asia/Seoul";
   public static final String DEFAULT_IMG_URL = "default_img_url";
+
+  public static final String RUNNING_WS_SEND_PREFIX = "/topic/runnings/";
 }
