6858298436696ab1f7547ba54f2aceb02dbfc74b
ekgns33 <ekgns00421@gmail.com>
2024-09-25 12:32:23 +0900

    :recycle: Refactor : apply Optional to return type

--- src/main/java/com/run_us/server/domains/running/repository/RunningRepository.java
+++ src/main/java/com/run_us/server/domains/running/repository/RunningRepository.java
@@ -1,6 +1,7 @@
 package com.run_us.server.domains.running.repository;
 
 import com.run_us.server.domains.running.domain.Running;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
@@ -9,5 +10,5 @@ import org.springframework.stereotype.Repository;
 public interface RunningRepository extends JpaRepository<Running, Long> {
 
   @Query("SELECT r FROM Running r WHERE r.publicKey = :publicKey")
-  Running findByPublicKey(String publicKey);
+  Optional<Running> findByPublicKey(String publicKey);
 }
