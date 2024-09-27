fe50b68b392c56f5dcee0f281b289b389a92c7b2
jeeheaG <jeehea99@naver.com>
2024-09-28 04:28:32 +0900

    :recycle: refactor : rename constant values of WebSocketConfig

--- src/main/java/com/run_us/server/global/common/GlobalConsts.java
+++ src/main/java/com/run_us/server/global/common/GlobalConsts.java
@@ -5,9 +5,10 @@ public final class GlobalConsts {
   public static final String DEFAULT_IMG_URL = "default_img_url";
 
   public static final String WS_CONNECT_ENDPOINT = "/ws-hello";
-  public static final String WS_PUB_DESTINATION_PREFIX = "/app";
-  public static final String WS_SUB_DESTINATION_PREFIX = "/topic";
-  public static final String WS_SUB_QUEUE_DESTINATION_PREFIX = "/queue";
+  public static final String WS_DESTINATION_PREFIX_APP = "/app";
+  public static final String WS_DESTINATION_PREFIX_TOPIC = "/topic";
+  public static final String WS_DESTINATION_PREFIX_QUEUE = "/queue";
+
   public static final String RUNNING_WS_SEND_PREFIX = "/topic/runnings/";
 
   public static final String WS_USER_AUTH_HEADER = "user-id";
--- src/main/java/com/run_us/server/global/config/WebSocketConfig.java
+++ src/main/java/com/run_us/server/global/config/WebSocketConfig.java
@@ -4,7 +4,6 @@ import com.run_us.server.domains.running.controller.StompInterceptor;
 import com.run_us.server.global.exceptions.StompErrorHandler;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
@@ -26,15 +25,16 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint(WS_CONNECT_ENDPOINT).setAllowedOriginPatterns("*"); // ws 연결 요청 - 모든 곳으로부터의 요청 허용
+        registry.addEndpoint(WS_CONNECT_ENDPOINT)
+                .setAllowedOriginPatterns("*"); // ws 연결 요청 - 모든 곳으로부터의 요청 허용
 
         registry.setErrorHandler(stompErrorHandler);
     }
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
-        registry.enableSimpleBroker(WS_SUB_DESTINATION_PREFIX, WS_SUB_QUEUE_DESTINATION_PREFIX); // 구독, 전송(sub)
-        registry.setApplicationDestinationPrefixes(WS_PUB_DESTINATION_PREFIX); // 발행(pub)
+        registry.enableSimpleBroker(WS_DESTINATION_PREFIX_TOPIC, WS_DESTINATION_PREFIX_QUEUE); // 구독, 전송(sub)
+        registry.setApplicationDestinationPrefixes(WS_DESTINATION_PREFIX_APP); // 발행(pub)
     }
 
     @Override
