a9cddef4833e91608ca2dc3dcabb8bf6535d535d
ekgns33 <ekgns00421@gmail.com>
2024-10-01 14:03:22 +0900

    :sparkles: Feat : add constant variables

--- src/main/java/com/run_us/server/global/common/GlobalConsts.java
+++ src/main/java/com/run_us/server/global/common/GlobalConsts.java
@@ -1,5 +1,9 @@
 package com.run_us.server.global.common;
 
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
 public final class GlobalConsts {
   public static final String TIME_ZONE_ID = "Asia/Seoul";
   public static final String DEFAULT_IMG_URL = "default_img_url";
@@ -10,6 +14,8 @@ public final class GlobalConsts {
   public static final String WS_DESTINATION_PREFIX_QUEUE = "/queue";
 
   public static final String RUNNING_WS_SEND_PREFIX = "/topic/runnings/";
+  public static final String RUNNING_WS_SUBSCRIBE_TOKEN = "runnings";
+  public static final String USER_WS_SUBSCRIBE_TOKEN = "queue";
 
   public static final String WS_USER_AUTH_HEADER = "user-id";
   public static final String SESSION_ATTRIBUTE_USER = "user-info";
