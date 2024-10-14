253c4545c3d23cf586ad0ede78ac443453f4f3b1
Daniel <ekgns00421@gmail.com>
2024-10-15 00:37:14 +0900

    :sparkles: Feat : add mapper class for service domain objects

---
+++ src/main/java/com/run_us/server/domains/running/service/model/RunningMapper.java
@@ -0,0 +1,38 @@
+package com.run_us.server.domains.running.service.model;
+
+import com.run_us.server.domains.running.controller.model.request.RunningRequest;
+import com.run_us.server.domains.running.domain.record.PersonalRecord;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public final class RunningMapper {
+
+  public static RunningAggregation toRunningAggregation(RunningRequest.AggregateRunning aggregateRunning) {
+    return RunningAggregation.builder()
+        .dataList(aggregateRunning.getDataList())
+        .count(aggregateRunning.getCount())
+        .runningDistanceInMeter(aggregateRunning.getRunningDistanceInMeter())
+        .runningDurationInMilliSecond(aggregateRunning.getRunningDurationInMilliSecond())
+        .averagePaceInMilliSecond(aggregateRunning.getAveragePaceInMilliSecond())
+        .build();
+  }
+
+  /***
+   * 개인기록 도메일 객체로 매핑하는 메서드
+   *
+   * @param userId 유저 id
+   * @param runningId 러닝 id
+   * @param aggregateRunning 러닝결과 집계
+   */
+  public static PersonalRecord toPersonalRecord(Long userId, Long runningId, RunningAggregation aggregateRunning) {
+    return PersonalRecord.builder()
+        .runningId(runningId)
+        .userId(userId)
+        .recordData(aggregateRunning.getDataList().stream().toString())
+        .runningDistanceInMeters(aggregateRunning.getRunningDistanceInMeter())
+        .runningDurationInMilliseconds(aggregateRunning.getRunningDurationInMilliSecond())
+        .averagePaceInMilliseconds(aggregateRunning.getAveragePaceInMilliSecond())
+        .build();
+  }
+}
