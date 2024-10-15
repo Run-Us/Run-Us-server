3f1a71e0943f6fc44966a9dc286bdc2fdc3e8c91
jeeheaG <jeehea99@naver.com>
2024-10-15 17:53:32 +0900

    :sparkles: feat : make ws session id header as const variable

--- src/main/java/com/run_us/server/global/common/SocketConst.java
+++ src/main/java/com/run_us/server/global/common/SocketConst.java
@@ -13,4 +13,6 @@ public final class SocketConst {
   public static final String USER_WS_SUBSCRIBE_PATH = "queue";
 
   public static final String USER_WS_ERROR_SUBSCRIBE_PATH = "/queue/errors";
+
+  public static final String WS_SESSION_ID_HEADER = "simpSessionId";
 }
