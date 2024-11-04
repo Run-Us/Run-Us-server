577e6150ece473eb5eabb4e349fada7e8f68ff20
ekgns33 <ekgns00421@gmail.com>
2024-11-04 13:58:49 +0900

    :white_check_mark: Test : add test for updating distance

--- src/test/java/com/run_us/server/domains/user/domain/UserRepositoryTest.java
+++ src/test/java/com/run_us/server/domains/user/domain/UserRepositoryTest.java
@@ -6,6 +6,8 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import com.run_us.server.domains.user.repository.UserRepository;
 import java.util.Optional;
+
+import jakarta.persistence.EntityManager;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
@@ -14,6 +16,9 @@ import org.springframework.transaction.annotation.Transactional;
 
 @DataJpaTest
 class UserRepositoryTest {
+
+  @Autowired
+  private EntityManager em;
 
   @Autowired
   private UserRepository userRepository;
@@ -41,8 +46,8 @@ class UserRepositoryTest {
   void remove_user() {
     //given
     User user = UserFixtures.getDefaultUserWithNickname("REMOVED_USER");
-    ReflectionTestUtils.setField(user, "id", 1);
-    ReflectionTestUtils.setField(user.getProfile(), "userId", 1);
+    ReflectionTestUtils.setField(user, "id", 2);
+    ReflectionTestUtils.setField(user.getProfile(), "userId", 2);
     user.remove();
     userRepository.save(user);
 
@@ -52,6 +57,27 @@ class UserRepositoryTest {
     //then
     assertNotNull(user.getDeletedAt());
     assertTrue(removedUser.isEmpty());
+  }
+
+  @Transactional
+  @Test
+  void update_user_running_info() {
+    //given
+    User user = UserFixtures.getDefaultUserWithNickname("UPDATE_USER");
+    userRepository.saveAndFlush(user);
+
+    //when
+    User savedUser = userRepository.findByNickname("UPDATE_USER").get();
+    savedUser.updateUserRunningInfo(1000, 1000);
+    em.flush();
+    em.clear();
+
+    // then
+    savedUser = userRepository.findByNickname("UPDATE_USER").get();
+    Profile userProfile = savedUser.getProfile();
+    assertEquals(1000, userProfile.getTotalDistance());
+    assertEquals(1000, userProfile.getTotalTime());
+    assertEquals(1000, user.getProfile().getTotalDistance());
   }
 
 }
