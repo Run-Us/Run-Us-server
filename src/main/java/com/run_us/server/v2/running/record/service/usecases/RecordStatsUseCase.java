318cd9ce6adc976e5d192a1d110d68d80a669db2
ekgns33 <ekgns00421@gmail.com>
2024-11-30 00:24:26 +0900

    :recycle: Refactor : remove outdated v2 directory

--- src/main/java/com/run_us/server/v2/running/record/service/usecases/RecordStatsUseCase.java
+++
@@ -1,9 +0,0 @@
-package com.run_us.server.v2.running.record.service.usecases;
-
-public interface RecordStatsUseCase {
-  void getUserWeeklyStats(Integer userId);
-
-  void getUserMonthlyStats(Integer userId);
-
-  void getUserYealyStats(Integer userId);
-}
