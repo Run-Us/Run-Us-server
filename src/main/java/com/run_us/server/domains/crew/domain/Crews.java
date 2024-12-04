b8a9cc46760e7f1dd9ceb483e2cae9b48277786f
jeeheaG <jeehea99@naver.com>
2024-12-04 21:32:16 +0900

    :sparkles: Add : make crew entity

---
+++ src/main/java/com/run_us/server/domains/crew/domain/Crews.java
@@ -0,0 +1,40 @@
+package com.run_us.server.domains.crew.domain;
+
+import com.run_us.server.domains.user.domain.User;
+import com.run_us.server.global.common.DateAudit;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import org.hibernate.annotations.SQLRestriction;
+
+import java.time.LocalDateTime;
+
+@ToString
+@Getter
+@NoArgsConstructor
+@Entity
+@Table(name = "crews")
+@SQLRestriction("deleted_at is null")
+public class Crews extends DateAudit {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "public_id", nullable = false, columnDefinition = "CHAR(13)")
+    private String publicId;
+
+    private String name;
+
+    private String description;
+
+    @ManyToOne
+    @JoinColumn(name = "organizer_id")
+    private User organizer;
+
+    @Column(name = "deleted_at")
+    private LocalDateTime deletedAt;
+
+    private CrewStatusEnum status;
+
+}
