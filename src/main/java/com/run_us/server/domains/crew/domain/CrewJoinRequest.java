9034e357733dfc2e83910d593f03f3b0bc982167
jeeheaG <jeehea99@naver.com>
2024-12-05 00:58:46 +0900

    :sparkles: add : make CrewJoinRequestStatusEnum entity

---
+++ src/main/java/com/run_us/server/domains/crew/domain/CrewJoinRequest.java
@@ -0,0 +1,43 @@
+package com.run_us.server.domains.crew.domain;
+
+import com.run_us.server.domains.user.domain.User;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import org.hibernate.annotations.SQLRestriction;
+
+import java.time.ZonedDateTime;
+
+@ToString
+@Getter
+@NoArgsConstructor
+@Entity
+@Table(name = "crew_join_requests")
+@SQLRestriction("deleted_at is null")
+public class CrewJoinRequest {
+    @Id
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "crew_id", nullable = false)
+    private Crew crew;
+
+    @Column(nullable = false)
+    private CrewJoinRequestStatusEnum status;
+
+    @Column(name = "requested_at", nullable = false)
+    private ZonedDateTime requestedAt;
+
+    @Column(name = "processed_at")
+    private ZonedDateTime processedAt;
+
+    @ManyToOne
+    @JoinColumn(name = "processed_by")
+    private User processedBy;
+
+}
---
+++ src/main/java/com/run_us/server/domains/crew/domain/CrewJoinRequestStatusEnum.java
@@ -0,0 +1,7 @@
+package com.run_us.server.domains.crew.domain;
+
+public enum CrewJoinRequestStatusEnum {
+    WAIT,
+    APPROVE,
+    REJECT;
+}
