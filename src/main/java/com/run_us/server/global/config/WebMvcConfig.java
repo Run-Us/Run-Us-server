e0adee13e04feed615439e16132575b2c8a82a1d
Hyeonjae.kim <hyeonjae.dev@gmail.com>
2024-12-18 22:13:50 +0900

    ✨ Feat : Implement @CurrentUser annotation

---
+++ src/main/java/com/run_us/server/global/config/WebMvcConfig.java
@@ -0,0 +1,22 @@
+package com.run_us.server.global.config;
+
+import com.run_us.server.global.security.resolver.CurrentUserArgumentResolver;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+    private final CurrentUserArgumentResolver currentUserResolver;
+
+    public WebMvcConfig(CurrentUserArgumentResolver currentUserResolver) {
+        this.currentUserResolver = currentUserResolver;
+    }
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        resolvers.add(currentUserResolver);
+    }
+}
---
+++ src/main/java/com/run_us/server/global/security/resolver/UserIdResolver.java
@@ -0,0 +1,39 @@
+package com.run_us.server.global.security.resolver;
+
+import com.run_us.server.domains.user.domain.User;
+import com.run_us.server.domains.user.exception.UserErrorCode;
+import com.run_us.server.domains.user.exception.UserException;
+import com.run_us.server.domains.user.repository.UserRepository;
+import com.run_us.server.global.common.cache.InMemoryCache;
+import com.run_us.server.global.security.principal.UserPrincipal;
+import org.springframework.stereotype.Component;
+
+import java.time.Duration;
+
+@Component
+public class UserIdResolver {
+    private final InMemoryCache<String, UserPrincipal> principalCache;
+    private final UserRepository userRepository;
+
+    private static final Duration CACHE_TTL = Duration.ofMinutes(30);
+
+    public UserIdResolver(InMemoryCache<String, UserPrincipal> principalCache,
+                          UserRepository userRepository) {
+        this.principalCache = principalCache;
+        this.userRepository = userRepository;
+    }
+
+    public UserPrincipal resolveUser(String publicId) {
+        return principalCache.get(publicId)
+                .orElseGet(() -> loadAndCacheUserPrincipal(publicId));
+    }
+
+    private UserPrincipal loadAndCacheUserPrincipal(String publicId) {
+        User user = userRepository.findByPublicId(publicId)
+                .orElseThrow(() -> new UserException(UserErrorCode.USER_NOT_FOUND));
+
+        UserPrincipal principal = new UserPrincipal(publicId, user.getId());
+        principalCache.put(publicId, principal, CACHE_TTL);
+        return principal;
+    }
+}
