6a5ec02e5f349b066e7da89a2ba3bdad0d7fc4d6
Hyeonjae.kim <hyeonjae.dev@gmail.com>
2024-12-19 13:55:59 +0900

    ♻️ Refactor : Update CrewErrorCode according to team convention

--- src/main/java/com/run_us/server/domains/crew/controller/model/enums/CrewErrorCode.java
+++ src/main/java/com/run_us/server/domains/crew/controller/model/enums/CrewErrorCode.java
@@ -5,16 +5,16 @@ import org.springframework.http.HttpStatus;
 
 public enum CrewErrorCode implements CustomResponseCode {
     // 404
-    CREW_NOT_FOUND("CEH4001", "Crew not found", "Crew not found", HttpStatus.NOT_FOUND),
-    JOIN_REQUEST_NOT_FOUND("CEH4002", "Join request not found", "Join request not found", HttpStatus.NOT_FOUND),
+    CREW_NOT_FOUND("CEH4041", "Crew not found", "Crew not found", HttpStatus.NOT_FOUND),
+    JOIN_REQUEST_NOT_FOUND("CEH4042", "Join request not found", "Join request not found", HttpStatus.NOT_FOUND),
 
     // 400
-    INVALID_JOIN_REQUEST("CEH4003", "Invalid join request", "Invalid join request", HttpStatus.BAD_REQUEST),
-    ALREADY_CREW_MEMBER("CEH4004", "User is already a crew member", "User is already a crew member", HttpStatus.BAD_REQUEST),
-    DUPLICATE_JOIN_REQUEST("CEH4005", "Duplicate join request", "Duplicate join request", HttpStatus.BAD_REQUEST),
+    INVALID_JOIN_REQUEST("CEH4001", "Invalid join request", "Invalid join request", HttpStatus.BAD_REQUEST),
+    ALREADY_CREW_MEMBER("CEH4002", "User is already a crew member", "User is already a crew member", HttpStatus.BAD_REQUEST),
+    DUPLICATE_JOIN_REQUEST("CEH4003", "Duplicate join request", "Duplicate join request", HttpStatus.BAD_REQUEST),
 
     // 403
-    NOT_CREW_OWNER("CEH4007", "User is not crew owner", "User is not crew owner", HttpStatus.FORBIDDEN)
+    NOT_CREW_OWNER("CEH4031", "User is not crew owner", "User is not crew owner", HttpStatus.FORBIDDEN)
     ;
 
     private final String code;
