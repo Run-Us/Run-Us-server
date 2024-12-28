ffe52d09b944d7bf55aea8b778727ad17f050034
Hyeonjae.kim <hyeonjae.dev@gmail.com>
2024-12-28 15:24:46 +0900

    🔧 Chore : Add missing Req/Res files

---
+++ src/main/java/com/run_us/server/domains/crew/controller/model/response/ReviewJoinRequestResponse.java
@@ -0,0 +1,10 @@
+package com.run_us.server.domains.crew.controller.model.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class ReviewJoinRequestResponse {
+    private Integer requestId;
+}
