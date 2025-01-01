fd92bbe593f683bb9324c8db0c0b22854b1cc22a
jeeheaG <jeehea99@naver.com>
2025-01-01 22:39:37 +0900

    :recycle: refactor : rename CommandCrewService

--- src/main/java/com/run_us/server/domains/crew/service/CommandCrewService.java
+++ src/main/java/com/run_us/server/domains/crew/service/CommandCrewService.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
-public class CrewCommandService {
+public class CommandCrewService {
 
     private final CrewRepository crewRepository;
 
