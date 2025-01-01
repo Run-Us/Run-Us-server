0a860052abbbea70ab70d8909bad5423cbe28083
jeeheaG <jeehea99@naver.com>
2025-01-01 16:50:38 +0900

    :sparkles: edit : edit get crew home and info page status code

--- src/main/java/com/run_us/server/domains/crew/controller/model/enums/CrewHttpResponseCode.java
+++ src/main/java/com/run_us/server/domains/crew/controller/model/enums/CrewHttpResponseCode.java
@@ -7,8 +7,8 @@ import org.springframework.http.HttpStatus;
 @RequiredArgsConstructor
 public enum CrewHttpResponseCode implements CustomResponseCode {
     CREW_CREATED("CSH2011", HttpStatus.CREATED, "크루 생성 성공", "크루 생성 성공"),
-    CREW_HOME_INFO_FETCHED("CSH2002", HttpStatus.OK, "크루 홈 데이터 조회 성공", "크루 홈 데이터 조회 성공"),
-    CREW_FETCHED("CSH2003", HttpStatus.OK, "크루 정보 조회 성공", "크루 정보 조회 성공"),
+    CREW_HOME_FETCHED("CSH2002", HttpStatus.OK, "크루 홈 조회 성공", "크루 홈 조회 성공"),
+    CREW_INFO_PAGE_FETCHED("CSH2003", HttpStatus.OK, "크루 정보 페이지 조회 성공", "크루 정보 페이지 조회 성공"),
     CREW_UPDATED("CSH2004", HttpStatus.OK, "크루 정보 수정 성공", "크루 정보 수정 성공"),
     CREW_JOIN_RULE_UPDATED("CSH2005", HttpStatus.OK, "크루 가입 방식 수정 성공", "크루 가입 방식 수정 성공"),
     CREW_SEARCH_SUCCESS("CSH2006", HttpStatus.OK, "크루 검색 성공", "크루 검색 성공"),
