22deb7dd649b36aa6abd3c7220c360f405e6244e
ekgns33 <ekgns00421@gmail.com>
2025-01-05 17:14:20 +0900

    :white_check_mark: Test : add repository test for crew join request

--- src/test/java/com/run_us/server/domains/crew/domain/CrewFixtures.java
+++ src/test/java/com/run_us/server/domains/crew/domain/CrewFixtures.java
@@ -12,17 +12,25 @@ public final class CrewFixtures {
 
     /**
      * 크루장이 크루를 처음 개설했을 때의 Crew 엔티티 반환
-     * @return
+     * @return Crew 엔티티
      */
     public static Crew createInitalCrew(String ownerNickname, String crewTitle, String crewIntro) {
         User owner = UserFixtures.getDefaultUserWithNickname(ownerNickname);
-        Crew crew = Crew.builder()
-                .owner(owner)
-                .crewDescription(createCrewDescription(crewTitle, crewIntro))
-                .crewMemberships(createOwnerOnlyCrewMemberships(owner))
-                .joinType(CrewJoinType.OPEN)
-                .build();
-        return crew;
+      return Crew.builder()
+              .owner(owner)
+              .crewDescription(createCrewDescription(crewTitle, crewIntro))
+              .crewMemberships(createOwnerOnlyCrewMemberships(owner))
+              .joinType(CrewJoinType.OPEN)
+              .build();
+    }
+
+    public static Crew createCrewWithOwner(User owner, String crewTitle, String crewIntro) {
+      return Crew.builder()
+              .owner(owner)
+              .crewDescription(createCrewDescription(crewTitle, crewIntro))
+              .crewMemberships(createOwnerOnlyCrewMemberships(owner))
+              .joinType(CrewJoinType.OPEN)
+              .build();
     }
 
     private static CrewDescription createCrewDescription(String crewTitle, String crewIntro) {
@@ -39,8 +47,8 @@ public final class CrewFixtures {
 
     /**
      * 크루장만 포함된 회원 목록 반환
-     * @param user
-     * @return
+     * @param user 크루장
+     * @return 크루장만 포함된 크루 멤버십 목록
      */
     private static List<CrewMembership> createOwnerOnlyCrewMemberships(User user) {
         CrewMembership ownerMembership = CrewMembership.builder()
