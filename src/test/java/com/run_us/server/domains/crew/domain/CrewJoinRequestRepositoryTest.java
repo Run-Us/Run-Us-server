65ccdf0ddb5015e6bfd9d2b6752df0ec88a6dd7b
ekgns33 <ekgns00421@gmail.com>
2025-01-11 18:28:33 +0900

    :white_check_mark: Test : add test profiles, refactor imports

--- src/test/java/com/run_us/server/domains/crew/domain/CrewJoinRequestRepositoryTest.java
+++ src/test/java/com/run_us/server/domains/crew/domain/CrewJoinRequestRepositoryTest.java
@@ -10,6 +10,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ActiveProfiles;
 
 import java.time.ZonedDateTime;
 
@@ -18,6 +19,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @DataJpaTest
+@ActiveProfiles("test")
 public class CrewJoinRequestRepositoryTest {
 
   @Autowired
